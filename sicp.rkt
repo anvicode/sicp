ca2168a9c55f3d2c525f7b3f8616774e75143a22
Andrey Vorontsov <reygofor@gmail.com>
2022-07-01 17:33:00 +0300

    chore: 1.1.4. Составные процедуры+

--- sicp.rkt
+++ sicp.rkt
@@ -4,5 +4,15 @@
 ;; ( + 145 980 )
 ;; 1.1.4. Составные процедуры
 (define (square x) (* x x))
-(square 21)
-(square (+ 2 5))
+;; (square 21)
+;; (square (+ 2 5))
+;;
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+;;
+;; (sum-of-squares 3 4) ;; 25
+;;
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(f 5) ;; 136
